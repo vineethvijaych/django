198856366c33e60e166a233c6d6dd76739724404
vineeth <vineethvijaysynnefo@gmail.com>
2023-07-10 08:54:07 +0530

    new files

--- django sample/pro/app1/models.py
+++ django sample/pro/app1/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class student(models.Model):
+    student_id =models.IntegerField()
+    student_name =models.CharField(max_length=20)
+    course_name =models.CharField(max_length=20)
+    
+    def __str__(self):
+        return self.student_name
--- pro/app1/models.py
+++ pro/app1/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class student(models.Model): 
+    id_no = models.IntegerField()
+    sname = models.CharField(max_length=20) 
+    cname = models.CharField(max_length=20,null=True)
+
+    def __str__(self):
+        return self.sname
+
+
+
+
